f7d6f74de875667162920f21f03635fb440f0b5e
AndriiKalinchuk <126283550+AndriiKalinchuk@users.noreply.github.com>
2023-05-10 19:47:52 +0300

    no-style-list

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const axios = require('axios').default;
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import axios from 'axios';
